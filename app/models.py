9321a1e6e673cdeee45a35f05057388324e17a49
joeneumann <joseph.r.neumann@icloud.com>
2016-08-08 20:50:14 -0500

    Implement intial Patient model

--- app/models.py
+++ app/models.py
@@ -17,6 +17,29 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True, nullable=False)
     email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    patient = db.relationship('Patient', backref='user', uselist=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Patient(db.Model):
+    __tablename__ = 'patient'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    first_name = db.Column(db.String(128), nullable=False, index=True)
+    last_name = db.Column(db.String(128), nullable=False, index=True)
+    middle_initial = db.Column(db.String(1), nullable=True)
+    dob = db.Column(db.Date,nullable=False, index=True)
+    gender = db.Column(db.String(1), nullable=True)
+    address1 = db.Column(db.String(256), nullable=True)
+    address2 = db.Column(db.String(256), nullable=True)
+    city = db.Column(db.String(128), nullable=True)
+    state = db.Column(db.String(2), nullable=True)
+    zip = db.Column(db.String(9), nullable=True)
+    ssn = db.Column(db.String(9), nullable=True, index=True)
+    phone_mobile = db.Column(db.String(16), nullable=True, index=True)
+    phone_home = db.Column(db.String(16), nullable=True)
+    email = db.Column(db.String(64), nullable=True, index=True)
+
+    def __repr__(self):
+        return '<Patient %r>' % self.id
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Patient
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -13,9 +13,16 @@ migrate = Migrate(app, db)
 
 #Create custom context with Flask-Script and set default import objects
 # To create/upgrade the databases:
+# $ python manage.py db migrate --m "Commit commet"
 # $ python manage.py db upgrade
 # $ heroku run python manage.py db upgrade --app unkani-staging
 # $ heroku run python manage.py db upgrade --app unkani
+
+#To recreate database from scratch
+#Drop all tables in postgres db console
+# $ heroku run python manage.py db downgrade base --app unkani
+# $ heroku run python manage.py db upgrade head --app unkani
+
 
 # To run as a shell:
 # $ python manage.py shell

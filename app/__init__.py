e62d09f9f9cac09dca0bfc496e76aec22fc2df29
joeneumann <joseph.r.neumann@icloud.com>
2016-08-10 10:20:54 -0500

    Update email support

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
 
 #Intialize Flask extensions, but do not specify application instance
@@ -11,6 +12,9 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 #Application factory function
 #Use app.config.from_object method to pull config dict / object
@@ -24,10 +28,14 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
-#Register blueprint object with application object
+#Register blueprint objects with application object
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
 
 
 class Role(db.Model):
@@ -11,16 +15,28 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True, nullable=False)
     email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     patient = db.relationship('Patient', backref='user', uselist=False)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError ('Password is not a readable attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password,method='pbkdf2:sha1',salt_length=8)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 class Patient(db.Model):
     __tablename__ = 'patient'
@@ -43,3 +59,8 @@ class Patient(db.Model):
 
     def __repr__(self):
         return '<Patient %r>' % self.id
+
+#Callback function, receives a user identifier and returns either user object or None
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))

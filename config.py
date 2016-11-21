0779255f7162df576079678940276d267b5a1bf7
joeneumann <joseph.r.neumann@icloud.com>
2016-11-21 10:42:24 -0600

    Fix configuration for Redis

--- config.py
+++ config.py
@@ -5,15 +5,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hfwznel2805lkd43o98udnj'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    # Old Flask_Mail Gmail Settings
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 465 #587 for TLS
-    # MAIL_USE_TLS = False
-    # MAIL_USE_SSL = True
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'app.unkani@gmail.com'
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # FLASKY_MAIL_SUBJECT_PREFIX = '[unkani]'
-    # FLASKY_MAIL_SENDER = 'unkani admin <unkani@gmail.com>'
     UNKANI_ADMIN = os.environ.get('UNKANI_ADMIN') or 'admin@unkani.com'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SENTRY_DSN = os.environ.get('SENTRY_DSN')
@@ -26,6 +17,8 @@ class Config:
     CELERY_BROKER_URL = os.environ.get('REDIS_URL')
     BROKER_URL = CELERY_BROKER_URL
     CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
+    CELERY_TASK_SERIALIZER = 'json'
+    BROKER_TRANSPORT = 'redis'
 
     # Define an init_app class method that allows for config specific initialization
     @staticmethod
@@ -36,11 +29,18 @@ class Config:
 # Define specific configuration variables as Config subclasses
 class DevelopmentConfig(Config):
     DEBUG = True
+    TESTING = False
 
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TESTING_DATABASE_URL')
+    WTF_CSRF_ENABLED = False
 
 class StagingConfig(Config):
     DEBUG = True
-    TESTING = True
+    TESTING = False
 
 
 class ProductionConfig(Config):
@@ -51,6 +51,7 @@ class ProductionConfig(Config):
 # Configuration objects are assigned to a dict for access in intitialization script from objects
 config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
     'staging': StagingConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig

216c853d31a77a5db5889ea4f696fb60f3089479
joeneumann <joseph.r.neumann@icloud.com>
2016-12-02 07:41:40 -0600

    Updated: refactor coverage and testrunner

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import os
+import subprocess
+import sys
 
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
@@ -42,27 +44,11 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
-
 @manager.command
-def test(coverage=False):
-    """Run the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE'):
-        import sys
-        os.environ['FLASK_COVERAGE'] = '1'
-        os.execvp(sys.executable, [sys.executable] + sys.argv)
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
+def test():
+    """Runs unit tests."""
+    tests = subprocess.call(['python', '-c', 'import tests; tests.run()'])
+    sys.exit(tests)
 
 
 @manager.command
@@ -70,6 +56,16 @@ def refresh_db():
     """Wipe db and reset with command line options
     for configuring admin user and random users"""
     reset_db_command_line()
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+    # migrate database to latest revision
+    upgrade()
+    # create user roles
+    Role.initialize_roles()
 
 
 if __name__ == '__main__':
---
+++ tests/__init__.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import unittest
+
+import coverage
+
+
+def run():
+    os.environ['FLASK_CONFIG'] = 'testing'
+
+    # start coverage engine
+    cov = coverage.coverage(branch=True, include=['app/*'], omit=['app/utils.py', 'app/flask_sendgrid.py'])
+    cov.start()
+
+    # run tests
+    tests = unittest.TestLoader().discover('tests')
+    ok = unittest.TextTestRunner(verbosity=2).run(tests).wasSuccessful()
+
+    # print coverage report
+    cov.stop()
+    cov.save()
+    print('Coverage Summary: ')
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'tmp/coverage')
+    print('HTML version: file://%s/index.html' % covdir)
+    cov.erase()
+
+    sys.exit(0 if ok else 1)

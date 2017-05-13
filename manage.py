ce356e1950cc8a249533a65be60d6fca1a073946
Joe Neumann <jneumann@aledade.com>
2017-05-12 20:13:52 -0500

    Update test command to have optional coverage html report output

--- manage.py
+++ manage.py
@@ -90,10 +90,25 @@ manager.add_command("celery-restart", CeleryWorkerRestart())
 
 
 @manager.command
-def test():
-    """Runs unit tests."""
-    tests = subprocess.call(['python', '-c', 'import tests; tests.run()'])
-    sys.exit(tests)
+def test(coverage=False):
+    """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 @manager.command
